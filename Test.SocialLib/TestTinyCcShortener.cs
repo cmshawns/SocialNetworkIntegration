63ff2314f25b83fbe3f99588053c4c6afa630257
Shawn S <shawn@codemastershawn.com>
2014-05-10 20:13:51 -0700

    Unit tests run w/ external config
    Got unit tests running (not passing) with workstation-specific
    config file (for private settings, etc.) As part of this, I
    created platform-specific build configurations with appropriate
    after-build custom commands.

--- Test.SocialLib/TestTinyCcShortener.cs
+++ Test.SocialLib/TestTinyCcShortener.cs
@@ -9,7 +9,6 @@ namespace Test.SocialLib
     [TestFixture()]
     public class TestTinyCcShortener
     {
-        // TODO: Store API key in a workstation-specific config file - BEFORE GITHUB
         private readonly string API_KEY = ConfigurationManager.AppSettings["API_tiny.cc"];
 
         [Test()]
